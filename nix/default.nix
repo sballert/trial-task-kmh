76161f500cd8c8956c8f8f0381e185a26999a5e1
sballert <sballert@posteo.de>
2022-09-16 20:07:56 +0200

    Setup pre-commit

--- nix/default.nix
+++ nix/default.nix
@@ -4,11 +4,30 @@ let
 
   pkgs = import sources.nixpkgs { };
 
+  pre-commit-hooks = (import sources."pre-commit-hooks.nix");
+
+  gitignoreSource = (import sources."gitignore.nix" { inherit (pkgs) lib; }).gitignoreSource;
+
+  src = gitignoreSource ../.;
+
 in
 {
   inherit pkgs;
 
   devTools = {
     inherit (pkgs) niv bashInteractive;
+    inherit (pre-commit-hooks) pre-commit;
+  };
+
+  ci = {
+    pre-commit-check = pre-commit-hooks.run {
+      inherit src;
+      hooks = {
+        shellcheck.enable = true;
+        nixpkgs-fmt.enable = true;
+        nix-linter.enable = true;
+      };
+      excludes = [ "^nix/sources\.nix$" ];
+    };
   };
 }
--- shell.nix
+++ shell.nix
@@ -2,4 +2,8 @@
 
 nix.pkgs.mkShell {
   buildInputs = builtins.attrValues nix.devTools;
+
+  shellHook = ''
+    ${nix.ci.pre-commit-check.shellHook}
+  '';
 }
