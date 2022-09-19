ba13364ea216173504e19138ba0caf6ba51016bf
sballert <sballert@posteo.de>
2022-09-19 12:23:57 +0200

    Add expressjs

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log('hello world')
+import express from "express";
+import log from "./logger";
+
+import routes from "./routes";
+
+const host = "localhost";
+const port = 3030;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, host, () => {
+  log.info(`Server listing at http://${host}:${port}`);
+
+  routes(app);
+});
+
+process.on("unhandledRejection", (reason, p) =>
+  log.error("Unhandled Rejection at: Promise ", p, reason),
+);
