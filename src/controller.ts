e8ceb60252c32f683524ed73c6687e6c0d3b54c3
sballert <sballert@posteo.de>
2022-09-19 23:52:55 +0200

    Move code into more functions

--- src/controller.ts
+++ src/controller.ts
@@ -10,6 +10,91 @@ import {
   getGenerallyAvailableProducts,
 } from "./service/product";
 import { isEmpty, uniqBy, find } from "lodash";
+import Product from "./model/product";
+import Customer from "./model/customer";
+
+function prepareProducts(
+  products: Product[],
+  isExistingCustomer: boolean,
+): any[] {
+  const preparedProducts = [];
+
+  for (const product of products) {
+    let price = product.prices;
+
+    if (Array.isArray(product.prices)) {
+      price = find(product.prices, (price) => {
+        return price.isExistingCustomer === isExistingCustomer;
+      });
+    }
+
+    preparedProducts.push({
+      productCode: product.productCode,
+      productCateogryCode: product.productCategoryCode,
+      productName: product.productName,
+      price: price.bruttoPrice,
+    });
+  }
+
+  return preparedProducts;
+}
+
+async function getProducts(customer: Customer | undefined): Promise<any[]> {
+  const generallyAvailableProducts = await getGenerallyAvailableProducts();
+
+  let products = generallyAvailableProducts;
+  let isExistingCustomer = false;
+
+  if (customer !== undefined) {
+    isExistingCustomer = true;
+
+    const availableProductsForCustomer = await getAvailabilitiesForCustomer(
+      customer,
+    );
+
+    if (!isEmpty(availableProductsForCustomer)) {
+      for (const availableProduct of availableProductsForCustomer) {
+        const product = await getProductByProductCode(availableProduct);
+        if (product !== undefined) {
+          products.push(product);
+        }
+      }
+    }
+  }
+
+  products = uniqBy(products, (product) => product.productCode);
+
+  return prepareProducts(products, isExistingCustomer);
+}
+
+function prepareRespond(
+  customer: Customer | undefined,
+  products: any,
+  customerId: string,
+): any {
+  if (customer !== undefined) {
+    return {
+      isExistingCustomer: true,
+      customer: {
+        customerId: customer.customerId,
+        gender: customer.gender,
+        givenName: customer.givenName,
+        surName: customer.surName,
+        birthDate: customer.birthDate,
+        isBusinessCustomer: customer.isBusinessCustomer,
+      },
+      products,
+    };
+  }
+
+  return {
+    isExistingCustomer: false,
+    customer: {
+      customerId,
+    },
+    products,
+  };
+}
 
 async function getProductsForCustomer(
   req: Request,
@@ -34,70 +119,9 @@ async function getProductsForCustomer(
       customer = await getCustomerByIsBusinessCustomer(isBusinessCustomer);
     }
 
-    const generallyAvailableProducts = await getGenerallyAvailableProducts();
+    const products = await getProducts(customer);
 
-    let products = generallyAvailableProducts;
-    let isExistingCustomer = false;
-
-    if (customer !== undefined) {
-      const availableProductsForCustomer = await getAvailabilitiesForCustomer(
-        customer,
-      );
-      isExistingCustomer = true;
-
-      if (!isEmpty(availableProductsForCustomer)) {
-        for (const availableProduct of availableProductsForCustomer) {
-          const product = await getProductByProductCode(availableProduct);
-          if (product !== undefined) {
-            products.push(product);
-          }
-        }
-      }
-    }
-
-    products = uniqBy(products, (product) => product.productCode);
-
-    const formatedProducts = [];
-
-    for (const product of products) {
-      let price = product.prices;
-
-      if (Array.isArray(product.prices)) {
-        price = find(product.prices, (price) => {
-          return price.isExistingCustomer === isExistingCustomer;
-        });
-      }
-
-      formatedProducts.push({
-        productCode: product.productCode,
-        productCateogryCode: product.productCategoryCode,
-        productName: product.productName,
-        price: price.bruttoPrice,
-      });
-    }
-
-    if (customer !== undefined) {
-      return res.send({
-        isExistingCustomer: true,
-        customer: {
-          customerId: customer.customerId,
-          gender: customer.gender,
-          givenName: customer.givenName,
-          surName: customer.surName,
-          birthDate: customer.birthDate,
-          isBusinessCustomer: customer.isBusinessCustomer,
-        },
-        products: formatedProducts,
-      });
-    }
-
-    return res.send({
-      isExistingCustomer: false,
-      customer: {
-        customerId,
-      },
-      products: formatedProducts,
-    });
+    res.send(prepareRespond(customer, products, customerId));
   } catch (e: any) {
     return res.status(500).json({
       status: false,
